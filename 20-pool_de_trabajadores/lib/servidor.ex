5b59b6a24f9b0edb14ee4badfdfb633dfc27b910
diego <diego@diego.diego>
2025-10-08 20:04:30 +0200

    Implementado pool de trabajadores

---
+++ 20-pool_de_trabajadores/lib/servidor.ex
@@ -0,0 +1,81 @@
+defmodule Servidor do
+  @moduledoc """
+  Servidor/master que coordina una pool de trabajadores
+  """
+
+  # Funciones públicas
+
+  def start(n) do
+    pid = spawn(fn -> init(n) end)
+    {:ok, pid}
+  end
+
+  def run_batch(master, jobs) do
+    send(master, {:trabajos, self(), jobs})
+
+    receive do
+      {:error, reason} -> {:error, reason}
+      results -> results
+    end
+  end
+
+  def stop(master) do
+    send(master, {:stop, self()})
+
+    receive do
+      :ok -> :ok
+    end
+  end
+
+  # Funciones privadas
+
+  defp init(n) do
+    workers = for _ <- 1..n, do: Trabajador.start()
+    loop(workers)
+  end
+
+  defp loop(workers) do
+    receive do
+      {:trabajos, from, trabajos} ->
+        reply = ejecutar_lote(trabajos, workers)
+        send(from, reply)
+        loop(workers)
+
+      {:stop, from} ->
+        Enum.each(workers, fn w -> send(w, :stop) end)
+        send(from, :ok)
+    end
+  end
+
+  defp ejecutar_lote(trabajos, workers) do
+    tandas = Enum.chunk_every(trabajos, length(workers))
+
+    tandas
+    |> Enum.with_index()
+    |> Enum.flat_map(fn {tanda, i} ->
+      ejecutar_tanda(tanda, workers, i * length(workers))
+    end)
+    |> Enum.sort_by(fn {idx, _} -> idx end)
+    |> Enum.map(fn {_, res} -> res end)
+  end
+
+  defp ejecutar_tanda(trabajos, workers, offset) do
+    etiquetados = Enum.with_index(trabajos, offset)
+    usados = Enum.take(workers, length(trabajos))
+
+    Enum.zip(etiquetados, usados)
+    |> Enum.each(fn {{func, idx}, worker} ->
+      send(worker, {:trabajo, self(), fn -> {idx, func.()} end})
+    end)
+
+    recolectar(length(trabajos), [])
+  end
+
+  defp recolectar(0, acc), do: acc
+  defp recolectar(n, acc) do
+    receive do
+      {:resultado, _pid, {idx, res}} ->
+        recolectar(n - 1, [{idx, res} | acc])
+    end
+  end
+end
--- 20-pool_de_trabajadores/test/pool_de_trabajadores_test.exs
+++ 20-pool_de_trabajadores/test/pool_de_trabajadores_test.exs
@@ -1,8 +1,52 @@
 defmodule PoolDeTrabajadoresTest do
   use ExUnit.Case
-  doctest PoolDeTrabajadores
 
-  test "greets the world" do
-    assert PoolDeTrabajadores.hello() == :world
+  test "inicia y detiene el servidor correctamente" do
+    {:ok, master} = Servidor.start(3)
+    assert is_pid(master)
+    assert Servidor.stop(master) == :ok
+  end
+
+  test "ejecuta un lote pequeño correctamente" do
+    {:ok, master} = Servidor.start(3)
+
+    jobs = [
+      fn -> 1 + 1 end,
+      fn -> Enum.sum(1..5) end,
+      fn -> :math.sqrt(16) end
+    ]
+
+    assert Servidor.run_batch(master, jobs) == [2, 15, 4.0]
+    Servidor.stop(master)
+  end
+
+  test "acepta lotes más grandes que el pool de trabajadores" do
+    {:ok, master} = Servidor.start(3)
+
+    jobs = Enum.map(1..10, fn i -> fn -> i * i end end)
+    assert Servidor.run_batch(master, jobs) == Enum.map(1..10, &(&1 * &1))
+
+    Servidor.stop(master)
+  end
+
+  test "mantiene el orden de resultados aunque los trabajos tarden mas o menos" do
+    {:ok, master} = Servidor.start(2)
+
+    jobs = [
+      fn -> :timer.sleep(200); "A" end,
+      fn -> "B" end,
+      fn -> "C" end,
+      fn -> :timer.sleep(100); "D" end
+    ]
+
+    assert Servidor.run_batch(master, jobs) == ["A", "B", "C", "D"]
+    Servidor.stop(master)
+  end
+
+  test "puede ejecutar varios lotes seguidos" do
+    {:ok, master} = Servidor.start(3)
+    assert Servidor.run_batch(master, [fn -> "uno" end]) == ["uno"]
+    assert Servidor.run_batch(master, [fn -> "dos" end, fn -> "tres" end]) == ["dos", "tres"]
+    Servidor.stop(master)
   end
 end
