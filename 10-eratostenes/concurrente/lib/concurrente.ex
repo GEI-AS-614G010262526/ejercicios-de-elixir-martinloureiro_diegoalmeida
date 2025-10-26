0aa10928ca880f6d11f831305a22e63a649ef245
MartinLoureiro <martin.loureirog@udc.es>
2025-10-26 22:30:34 +0100

    Corrección 10-eratostenes /concurrente

--- 10-eratostenes/concurrente/lib/concurrente.ex
+++ 10-eratostenes/concurrente/lib/concurrente.ex
@@ -1,41 +1,28 @@
 defmodule Filtro do
-  def start(primo, siguiente_pid, original_sink_pid) do
-    spawn(__MODULE__, :loop, [primo, siguiente_pid, original_sink_pid])
+  def start(primo, siguiente_pid, sink_pid) do
+    spawn(__MODULE__, :loop, [primo, siguiente_pid, sink_pid])
   end
 
-  def loop(primo, siguiente_pid, original_sink_pid) do
+  def loop(primo, siguiente_pid, sink_pid) do
     receive do
-      {:numero, n} ->
-        if rem(n, primo) == 0 do
-          loop(primo, siguiente_pid, original_sink_pid)
-        else
-          # Si no es divisible
-          if siguiente_pid == original_sink_pid do
-            # 'n' es un nuevo primo.
-            
-            # Notificar al Sink que 'n' es un primo para que lo acumule.
-            send(original_sink_pid, {:numero, n}) 
-            
-            # Crear un nuevo filtro para 'n' y engancharlo a la cadena.
-            nuevo_filtro_pid = Filtro.start(n, original_sink_pid, original_sink_pid)
-            
-            # Actualizar su 'siguiente_pid' para apuntar al nuevo filtro.
-            loop(primo, nuevo_filtro_pid, original_sink_pid)
-          else
-            # Si el siguiente es otro filtro, simplemente le enviamos el número
-            send(siguiente_pid, {:numero, n})
-            loop(primo, siguiente_pid, original_sink_pid)
-          end
-        end
+      # Si el número es divisible por mi primo, lo descarto y continúo
+      {:numero, n} when rem(n, primo) == 0 ->
+        loop(primo, siguiente_pid, sink_pid)
 
-      {:get_primos, sender_pid} ->
-        send(siguiente_pid, {:get_primos, sender_pid})
+      # Si soy el último filtro (apunto al Sink), 'n' es un nuevo primo
+      {:numero, n} when siguiente_pid == sink_pid ->
+        send(sink_pid, {:nuevo_primo, n, self()})
         receive do
-          {:primos_recopilados, tail_primos} ->
-            send(sender_pid, {:primos_recopilados, [primo | tail_primos]})
+          {:actualizado, nuevo_siguiente_pid} ->
+            loop(primo, nuevo_siguiente_pid, sink_pid)
         end
-        loop(primo, siguiente_pid, original_sink_pid)
 
+      # Si no es divisible, simplemente paso el número al siguiente filtro de la cadena
+      {:numero, n} ->
+        send(siguiente_pid, {:numero, n})
+        loop(primo, siguiente_pid, sink_pid)
+
+      # Al recibir la señal de fin, la propago por la cadena
       :fin ->
         send(siguiente_pid, :fin)
     end
@@ -44,45 +31,48 @@ end
 
 defmodule Sink do
   def start(pid_principal) do
-    spawn(__MODULE__, :loop, [pid_principal, []])
+    spawn(__MODULE__, :loop, [pid_principal, [2]])
   end
 
   def loop(pid_principal, primos_acumulados) do
     receive do
-      {:numero, n} ->
-        # El Sink acumula los números primos que le llegan.
-        loop(pid_principal, primos_acumulados ++ [n])
+      {:nuevo_primo, n, ultimo_filtro_pid} ->
+        # Creo el nuevo filtro, que apuntará al Sink
+        nuevo_filtro_pid = Filtro.start(n, self(), self())
+        # Le respondo al filtro anterior para que actualice su 'siguiente_pid'
+        send(ultimo_filtro_pid, {:actualizado, nuevo_filtro_pid})
+        # Acumulo el nuevo primo y continúo.
+        loop(pid_principal, [n | primos_acumulados])
 
-      {:get_primos, sender_pid} ->
-        send(sender_pid, {:primos_recopilados, primos_acumulados})
-        loop(pid_principal, primos_acumulados)
-
+      # La señal de fin ha llegado al final de la cadena
       :fin ->
-        send(pid_principal, {:primos_encontrados, primos_acumulados})
+        # Invierto la lista para ordenarla y la envío al proceso principal
+        primos_ordenados = Enum.reverse(primos_acumulados)
+        send(pid_principal, {:primos_encontrados, primos_ordenados})
     end
   end
 end
 
 defmodule EratostenesPipe do
-  @moduledoc """
-  Criba de Eratostenes usando procesos encadenados (pipe de filtros).
-  """
-
   def primos(n) when n < 2, do: []
-  def primos(2), do: [2] # <-- Añadir una función de guarda específica para n=2
+  def primos(2), do: [2]
+
   def primos(n) do
     pid_principal = self()
+    # Creo el Sink
     sink_pid = Sink.start(pid_principal)
-
+    # Creo el filtro para el primo 2
     primer_filtro_pid = Filtro.start(2, sink_pid, sink_pid)
-    send(sink_pid, {:numero, 2}) # El primo 2 se envía al Sink porque es el primer primo.
 
-    Enum.each(3..n, fn x -> # Envía cada número secuencialmente al `primer_filtro_pid`
-      send(primer_filtro_pid, {:numero, x})
-    end)
+    # Envío todos los números candidatos al principio de la cadena de filtros
+    for num <- 3..n do
+      send(primer_filtro_pid, {:numero, num})
+    end
 
+    # Envío la señal de finalización para que la cadena se cierre y devuelva el resultado
     send(primer_filtro_pid, :fin)
 
+    # Espero a que el Sink me envíe la lista final de primos encontrados
     receive do
       {:primos_encontrados, primos} -> primos
     end
